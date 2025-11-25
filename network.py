b85005872aeb92bde9f6b0c05953c84cc531cd16
rihem <rihamghoudi@gmail.com>
2025-11-25 23:21:04 +0100

    first step

--- network.py
+++ network.py
@@ -1,2 +1,12 @@
-n = int(input("donner un entier"))
-print(n)
+import requests
+def check_internet():
+    try:
+        requests.get("https://www.google.com",timeout=5)
+        print("internet is working")
+    except requests.ConnectionError:
+        print("no internet connection")
+    if __name__ == "__main__":
+        check_internet()
+
+            
+
