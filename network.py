7feacb35846e80f8207023f0e4136c9c7edb1346
rihem <rihamghoudi@gmail.com>
2025-11-26 14:22:39 +0100

    the new version code

--- network.py
+++ network.py
@@ -1,12 +1,30 @@
 import requests
+from datetime import datetime
+
+
 def check_internet():
-    try:
-        requests.get("https://www.google.com",timeout=5)
-        print("internet is working")
-    except requests.ConnectionError:
-        print("no internet connection")
-    if __name__ == "__main__":
-        check_internet()
+    sites = ["https://www.google.com",
+             "https://www.cloudflare.com",
+             "https://www.github.com"]
+    for site in sites:
+        try:
+            requests.get(site, timeout=5)
+            return True
+        except:
+            continue
+    return False
 
-            
 
+def write_log(status):
+    time_now = datetime.now().strftime("%Y-%m-%d%H:%M:%S")
+    with open("log.txt", "a") as file:
+        file.write(f"{time_now} - {status}\n")
+
+
+if __name__ == "__main__":
+    if check_internet():
+        message = "internet connected"
+    else:
+        message = "internet disconnected"
+    print(message)
+    write_log(message)
